90edd17b56f8c0a8335baa9d25f7e1fde4b5482a
SmitHumber <yellowpieorangesword14@gmail.com>
2026-05-22 07:51:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+// Declare and Use Variables
+
+var studentName = "Smit Patel";
+let studentAge = "22";
+const isEnrolled = "true";
+
+console.log(studentName);
+console.log(studentAge);
+console.log(isEnrolled);
+
+// Demonstrate Data Types
+
+let greeting = "Hello World";
+console.log(greeting);
+
+let distance = 29.9792458;
+console.log(distance);
+
+let getIphone15 = true;
+let getIphone9 = false;
+
+console.log(getIphone15);
+console.log(getIphone9);
+
+
+let codes = ["HTML", "CSS", "JS"];
+console.log(codes);
+
+
+let person = {
+    firstName: "Smit",
+    lastName: "Patel",
+    HumberID: "N01617816",
+};
+
+console.log(person);
+
+
+// Use Basic Operators
+
+console.log(1 + 1);
+console.log(2 - 1);
+console.log(2 * 2);
+console.log(10 / 2);
+
+
+let a = 20;
+let b = 5;
+
+
+console.log(a < b);
+console.log(a > b);
+console.log(a <= b);
+console.log(a >= b);
+console.log(a != b);
+console.log("20" == a);
+console.log("20" === 3);
+
+let x = true;
+let y = false;
+
+console.log(x && y);
+console.log(x || y);
+console.log(!x)
